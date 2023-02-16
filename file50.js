b0714a7ca36a7ff3c1c5a1382f0f75914449ebe5
Shammunul Islam <sha_is13@yahoo.com>
2023-02-16 13:16:48 +0600

    file50.js added

--- file50.js
+++ file50.js
@@ -9,5 +9,14 @@ const user1 = {
 
 // don't do the following mistake
 
-const myFunc = user1.about();
+const myFunc = user1.about; // this is not bounded yet
 myFunc(); // returns undefined undefined
+// here this value is window object
+
+
+
+// do the following if you have to save the function inside an object as a variable
+// if we want to bind this to user1 object, we need to use bind method
+
+const myFunc2 = user1.about.bind(user1);
+myFunc2(); // returns Shammunul 37
